a6d137eb5c6e25e46413a31f44cb26306674f5e9
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-09-24 01:09:30 -0400

    ReadMe setup

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,14 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://Abdullah:123@devconnector-gy13i.mongodb.net/test?retryWrites=true",
+  mongoURI: "YOUR_MONGODB_URL",
   secretOrKey: "secret",
   cloudinary: {
-    cloud_name: "dxemu0gku",
-    api_key: "332294393253119",
-    api_secret: "Cgf4N5fEfbtVj4gP9hNc82MI-Pw"
+    cloud_name: "YOUR_CLOUDINARY_CLOUD_NAME",
+    api_key: "YOUR_CLOUDINARY_API_KEY",
+    api_secret: "YOUR_CLOUDINARY_API_SECRET"
   },
   avatarPlaceholderUrl:
     "https://res.cloudinary.com/dxemu0gku/image/upload/v1557829466/avatar-placeholder_knb8nt.gif",
   API_URL: process.env.API_URL || "http://localhost:5000",
   FRONT_API_URL: process.env.API_URL || "http://localhost:3000",
-  sendgrid_api_key:
-    "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"
+  sendgrid_api_key: "YOUR_SENDGRID_API_KEY"
 };
